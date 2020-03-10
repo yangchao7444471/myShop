bba0bdac79a45af060debb73362cb467708d49a8
yangchaoqun <707359099@qq.com>
2020-03-10 13:06:45 +0800

    springCloud框架搭建

---
+++ qf-shop-product-service/src/main/java/com/qf/QfShopProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.qf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QfShopProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QfShopProductServiceApplication.class, args);
+	}
+
+}
